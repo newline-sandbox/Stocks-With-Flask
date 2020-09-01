45b14671ad6bb5014622b3d62345ca360d56eef4
Ten <nomadhideout@yandex.com>
2020-09-01 21:09:03 +0200

    restructure app, introduce a blueprint

--- blueprints/stock.py
+++ blueprints/stock.py
@@ -3,6 +3,11 @@ import requests
 
 stock = Blueprint("stock", __name__, url_prefix="/stocks")
 
+@stock.route("/<string:ticker>")
+def view_stock(ticker):
+  stock_price = fetch_price(ticker)
+  return render_template("stock_quote.html", ticker=ticker, stock_price=stock_price)
+
 def fetch_price(ticker):
   data = requests.get("https://financialmodelingprep.com/api/v3/stock/real-time-price/{}".format(ticker.upper()), params={"apikey": "7ee6a284877bac2cf363374463688fcd"}).json() 
   return data["price"]  
--- server.py
+++ server.py
@@ -1,16 +1,13 @@
 from flask import Flask, render_template, redirect, request, url_for
 import requests
+from blueprints.stock import stock
 
 app = Flask(__name__)
+app.register_blueprint(stock)
 
 @app.route("/")
 def index():
   return render_template("home.html")
-
-@app.route("/stocks/<string:ticker>")
-def view_stock(ticker):
-  stock_price = fetch_price(ticker)
-  return render_template("stock_quote.html", ticker=ticker, stock_price=stock_price)
 
 def fetch_price(ticker):
   data = requests.get("https://financialmodelingprep.com/api/v3/stock/real-time-price/{}".format(ticker.upper()), params={"apikey": "7ee6a284877bac2cf363374463688fcd"}).json() 
